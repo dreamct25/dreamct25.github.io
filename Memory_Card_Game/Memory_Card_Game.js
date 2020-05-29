40cc11a809129b745c38eb048ee78d692c9966a4
dream <h199410@gmail.com>
2020-05-29 22:07:52 +0800

    update

---
+++ Memory_Card_Game/Memory_Card_Game.js
@@ -0,0 +1,177 @@
+var cards = document.querySelectorAll('.card');
+var num = document.querySelector('.count')
+var cardTimes = 0;
+var hasOpen_Card = false;
+var cardFrt, cardScd;
+var lock = false;
+var btnClose = document.querySelector('.close')
+var modalShow = document.querySelector('.modal')
+var modalContentShow = document.querySelector('.modal-body')
+var again = document.querySelector('.again')
+var cancel = document.querySelector('.cancel')
+var shows;
+
+function cardChage() {
+    if (lock == true) {
+        return
+    }
+    if (this === cardFrt) return;
+    this.classList.add('card-turn')
+    if (!hasOpen_Card) {
+        hasOpen_Card = true
+        cardFrt = this
+        return
+    }
+    cardScd = this
+    cardMatch()
+}
+
+function cardMatch() {
+    var cardMatching = cardFrt.dataset.target === cardScd.dataset.target;
+    if (cardMatching == true) {
+        disableEvent()
+        return
+    }
+    returnCover()
+}
+
+function disableEvent() {
+    cardTimes += 1
+    if (cardTimes == 8) {
+        cardTimes = 0
+        modalHeader_Text()
+        modalContent_Text()
+        modalBtn_Cancel_TextOpen()
+        modalBtn_Again_Text()
+        modalShow.style.display = 'block'
+        modalShow.style.transition = '1s ease'
+        shows = setTimeout(show, 100)
+    }
+    cardFrt.removeEventListener('click', cardChage)
+    cardScd.removeEventListener('click', cardChage)
+    reset()
+}
+
+function returnCover() {
+    lock = true
+    setTimeout(function() {
+        cardFrt.classList.remove('card-turn')
+        cardScd.classList.remove('card-turn')
+        reset()
+    }, 1000)
+}
+
+function reset() {
+    [hasOpen_Card, lock] = [false, false]
+    [cardFrt, cardScd] = [null, null]
+}
+
+function resetAll() {
+    cards.forEach(function(card) {
+        var resetBoard = Math.floor(Math.random() * 16)
+        card.style.order = resetBoard
+    })
+    cards.forEach(function(carder) {
+        carder.classList.remove('card-turn')
+        carder.addEventListener('click', cardChage)
+    })
+
+}
+
+function modalHeader_Text() {
+    var modalText_header = document.querySelector('.header-text')
+    if (modalText_header.textContent == '') {
+        modalText_header.innerHTML = '遊戲說明'
+    } else if (modalText_header.textContent == '遊戲說明') {
+        modalText_header.innerHTML = '您要再來一局嗎'
+    } else {
+        modalText_header.innerHTML = '您要再來一局嗎'
+    }
+}
+
+function modalContent_Text() {
+    var modalText_content = document.querySelector('.content-text')
+    if (modalText_content.textContent == '') {
+        modalText_content.innerHTML = '點擊卡片對應相同的卡片的圖案<br>直到將整面卡面配對正確'
+    } else if (modalText_content.textContent == '點擊卡片對應相同的卡片的圖案<br>直到將整面卡面配對正確') {
+        modalText_content.innerHTML = '您已將卡牌全數對應成功瞜，恭喜 !<br>雖然只是個小遊戲，還是很感謝您的遊玩 !'
+    } else {
+        modalText_content.innerHTML = '您已將卡牌全數對應成功瞜，恭喜 !<br>雖然只是個小遊戲，還是很感謝您的遊玩 !'
+    }
+}
+
+function modalBtn_Cancel_TextClose() {
+    var cancelText = document.querySelector('.cancel-text')
+    if (cancel.classList == 'cancel') {
+        cancel.classList.remove('cancel')
+    }
+}
+
+function modalBtn_Cancel_TextOpen() {
+    var cancelText = document.querySelector('.cancel-text')
+    if (cancelText.textContent == '' && cancel.classList == '') {
+        cancel.classList.add('cancel')
+        cancelText.innerHTML = '取消'
+    }
+}
+
+function modalBtn_Again_Text() {
+    var againText = document.querySelector('.again-text')
+    if (againText.textContent == '') {
+        againText.innerHTML = '確定'
+    } else if (againText.textContent == '確定') {
+        againText.innerHTML = '再來一局'
+    } else {
+        againText.innerHTML = '再來一局'
+    }
+}
+// 設定淡入延遲
+function show() {
+    modalShow.classList.add('modal-toggle')
+    modalContentShow.classList.add('modal-body-toggle')
+}
+// 設定淡出延遲
+function close() {
+    modalShow.style.display = 'none'
+}
+
+cards.forEach(function(card) {
+    card.addEventListener('click', cardChage)
+})
+window.addEventListener('load', function() {
+        modalShow.style.display = 'block'
+        modalShow.style.transition = '1s ease'
+        shows = setTimeout(show, 100)
+        modalHeader_Text()
+        modalContent_Text()
+        modalBtn_Again_Text()
+        modalBtn_Cancel_TextClose()
+    })
+    // 監聽點擊彈出視窗本體內部架構 X 按鈕
+btnClose.addEventListener('click', function() {
+    modalShow.classList.remove('modal-toggle')
+    modalContentShow.classList.remove('modal-body-toggle')
+    shows = setTimeout(close, 750)
+})
+
+// 監聽點擊彈出視窗本體內部架構 Cancel 按鈕
+again.addEventListener('click', function() {
+    modalShow.classList.remove('modal-toggle')
+    modalContentShow.classList.remove('modal-body-toggle')
+    shows = setTimeout(close, 750)
+    resetAll()
+})
+cancel.addEventListener('click', function() {
+    modalShow.classList.remove('modal-toggle')
+    modalContentShow.classList.remove('modal-body-toggle')
+    shows = setTimeout(close, 750)
+})
+
+// 監聽點擊彈出視窗本體架構外部網頁空白區域 window (整個網頁當前視窗)，當點擊外部區域時一樣可關閉彈出視窗
+window.addEventListener('click', function(e) {
+    if (e.target == modalShow) {
+        modalShow.classList.remove('modal-toggle')
+        modalContentShow.classList.remove('modal-body-toggle')
+        shows = setTimeout(close, 750)
+    }
+})
