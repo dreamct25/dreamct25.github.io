f475902791c324cd058b321faf2df84579c445f3
dreamct25 <h199410@gmail.com>
2021-08-27 16:41:30 +0800

    upload origin file of React

--- store_origin/src/components/Main/index.js
+++
@@ -1,7 +0,0 @@
-import reducer from '../Main/reducer'
-import typesName from '../Main/actionType'
-import actions from '../Main/actionCreater'
-import styles from '../Main/styles'
-
- //eslint-disable-next-line
-export default { reducer,typesName,actions,styles }
