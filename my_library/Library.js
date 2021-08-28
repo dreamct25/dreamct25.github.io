843935a065c54faa0b7ac3175cad22ae7c277f83
dreamct25 <h199410@gmail.com>
2021-08-28 17:47:41 +0800

    update ver 1.0.3

--- my_library/Library.js
+++ my_library/Library.js
@@ -35,45 +35,92 @@
 //     val:Function = (valTemp:string | number) => valTemp === undefined ? this.value : this.value = valTemp
 // }
 
+// TypeScript function 版，2021/8/28
+// const $: any = (function (el) {
+//     const $ = (targets: any) => {
+//         let targetThis: { [key: string]: any } = el.call(el, targets) || targets
+//         targetThis.targets = targets
+//         targetThis.text = (txt: string | undefined) => txt === undefined ? targetThis.textContent : targetThis.textContent = txt
+//         targetThis.html = (dom: string) => targetThis.innerHTML = dom;
+//         targetThis.addClass = (classText: string) => targetThis.classList.add(classText);
+//         targetThis.removeClass = (classTxt: string) => targetThis.classList.remove(classTxt);
+//         targetThis.listener = (eventType: string, fn: Function) => targetThis.addEventListener(eventType, fn);
+//         targetThis.val = (valTemp: string | undefined) => valTemp === undefined ? targetThis.value : targetThis.value = valTemp;
+//         targetThis.attr = (props: string, val: any) => val === undefined ? targetThis.getAttribute(props) : targetThis.setAttribute(props, val);
+//         targetThis.styles = (cssType: string, cssParameter: string) => targetThis.style.setProperty(cssType, cssParameter);
+//         return targetThis
+//     }
+//     return $
+// }((el: object | string): any => typeof el === "object" ? el : document.querySelector(el)))
+//
+// $.each = (item: {}[] | [], fn: Function,) => item.forEach((items: any, index: number) => fn.call(item, items, index))
+// $.maps = (item: {}[] | [], fn: Function) => item.map((items: any, index: number) => fn.call(item, items, index))
+// $.filter = (item: {}[] | [], fn: Function) => item.filter((items: any) => fn.call(item, items))
+// $.indexOf = (item: any, x: never): number => item.indexOf(x)
+// $.includes = (item: any, x: never): boolean => item.includes(x)
+// $.findIndexOfObj = (item: any, fn: Function): number => item.findIndex((where: object) => fn.call(item, where))
+// $.sum = (item: any, fn: Function) => item.reduce((a: any, b: any) => fn.call(item, a, b))
+// $.eachKeys = (item: object) => Object.keys(item)
+
 // Javascript function 版，不需要 new 2021/7/29
-const element = (function () {
-    function element (el) {
-        this.el = el;
-        return typeof (el) === "object" ? el : document.querySelector(el);
-    }
-    return element;
-}());
+// const element = (function () {
+//     function element (el) {
+//         this.el = el;
+//         return typeof (el) === "object" ? el : document.querySelector(el);
+//     }
+//     return element;
+// }());
 
-const $ = (function (supers) {
-    function $(el){
-        let scopeThis = supers.call(this, el) || this;
-        scopeThis.el = el;
-        scopeThis.text = txt => txt === undefined ? scopeThis.textContent : scopeThis.textContent = txt;
-        scopeThis.html = dom => scopeThis.innerHTML = dom;
-        scopeThis.addClass = classText => scopeThis.classList.add(classText);
-        scopeThis.removeClass = classTxt => scopeThis.classList.remove(classTxt);
-        scopeThis.listener = (eventType, fn) => scopeThis.addEventListener(eventType, fn);
-        scopeThis.val = valTemp => valTemp === undefined ? scopeThis.value : scopeThis.value = valTemp;
-        scopeThis.attr = (props,val) => val === undefined ? scopeThis.getAttribute(props) : scopeThis.setAttribute(props,val);
-        scopeThis.styles = (cssType, cssParameter) => scopeThis.style.setProperty(cssType, cssParameter);
-        return scopeThis;
-    }
-    
-    return $
-}(element));
+// const $ = (function (supers) {
+//     function $(el){
+//         let scopeThis = supers.call(this, el) || this;
+//         scopeThis.el = el;
+//         scopeThis.text = txt => txt === undefined ? scopeThis.textContent : scopeThis.textContent = txt;
+//         scopeThis.html = dom => scopeThis.innerHTML = dom;
+//         scopeThis.addClass = classText => scopeThis.classList.add(classText);
+//         scopeThis.removeClass = classTxt => scopeThis.classList.remove(classTxt);
+//         scopeThis.listener = (eventType, fn) => scopeThis.addEventListener(eventType, fn);
+//         scopeThis.val = valTemp => valTemp === undefined ? scopeThis.value : scopeThis.value = valTemp;
+//         scopeThis.attr = (props,val) => val === undefined ? scopeThis.getAttribute(props) : scopeThis.setAttribute(props,val);
+//         scopeThis.styles = (cssType, cssParameter) => scopeThis.style.setProperty(cssType, cssParameter);
+//         return scopeThis;
+//     }
+//     return $
+// }(element));
 
-$.each = (item,fn) => item.forEach((items,index)=> fn.call(this,items,index))
+// $.each = (item,fn) => item.forEach((items,index)=> fn.call(this,items,index))
+// $.maps = (item,fn) => item.map((items,index)=> fn.call(this,items,index))
+// $.indexOf = (item,x) => item.indexOf(x)
+// $.findIndexOfObj = (item,fn) => item.findIndex(where => fn.call(this,where))
+// $.sum = (item,fn) => item.reduce((a,b)=> fn.call(this,a,b))
+// $.eachKeys = item => Object.keys(item)
 
-$.maps = (item,fn) => item.map((items,index)=> fn.call(this,items,index))
+// Javascript function 版，不需要 new 更新方法 2021/8/28
+const $ = (function (el) {
+    const $ = (targets) => {
+        let targetThis = el.call(el, targets) || targets;
+        targetThis.targets = targets;
+        targetThis.text = (txt) => txt === undefined ? targetThis.textContent : targetThis.textContent = txt;
+        targetThis.html = (dom) => targetThis.innerHTML = dom;
+        targetThis.addClass = (classText) => targetThis.classList.add(classText);
+        targetThis.removeClass = (classTxt) => targetThis.classList.remove(classTxt);
+        targetThis.listener = (eventType, fn) => targetThis.addEventListener(eventType, fn);
+        targetThis.val = (valTemp) => valTemp === undefined ? targetThis.value : targetThis.value = valTemp;
+        targetThis.attr = (props, val) => val === undefined ? targetThis.getAttribute(props) : targetThis.setAttribute(props, val);
+        targetThis.styles = (cssType, cssParameter) => targetThis.style.setProperty(cssType, cssParameter);
+        return targetThis;
+    };
+    return $;
+}((el) => typeof el === "object" ? el : document.querySelector(el)));
 
-$.indexOf = (item,x) => item.indexOf(x)
-
-$.findIndexOfObj = (item,fn) => item.findIndex(where => fn.call(this,where))
-
-$.sum = (item,fn) => item.reduce((a,b)=> fn.call(this,a,b))
-
-$.eachKeys = item => Object.keys(item)
-
+$.each = (item, fn) => item.forEach((items, index) => fn.call(item, items, index));
+$.maps = (item, fn) => item.map((items, index) => fn.call(item, items, index));
+$.filter = (item, fn) => item.filter((items) => fn.call(item, items));
+$.indexOf = (item, x) => item.indexOf(x);
+$.includes = (item, x) => item.includes(x);
+$.findIndexOfObj = (item, fn) => item.findIndex((where) => fn.call(item, where));
+$.sum = (item, fn) => item.reduce((a, b) => fn.call(item, a, b));
+$.eachKeys = (item) => Object.keys(item);
 
 
 Date.prototype.getFullDateTime = function(formatType) {
