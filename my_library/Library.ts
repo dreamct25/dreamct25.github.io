3e514ed6fd5c61bc17f567356afd6d6d8b01a127
Alex Chen <62322279+dreamct25@users.noreply.github.com>
2022-01-31 01:33:21 +0800

    update ver 1.3.4

--- my_library/Library.ts
+++ my_library/Library.ts
@@ -1,39 +1,59 @@
-// CopyRight by Chen 2021/08 - 2022/01 Library language - typescript ver 1.3.3
-// work environment typescript v4.5.4
+// CopyRight by Chen 2021/08 - 2022/01 Library language - typescript ver 1.3.4
+// Work Environment Typescript v4.5.4
 //
 // Use in node js
 // export default $
 const $: any = (function (el) {
-    const $ = (targets: any): any => {
-        let targetThis: any = el.call(el, targets) || targets;
-        targetThis.targets = targets;
-        targetThis.texts = (txt: string | undefined): string | void => txt === undefined ? targetThis.textContent : targetThis.textContent = txt;
-        targetThis.html = (dom: string | undefined): string | void => dom === undefined ? targetThis.innerHTML : targetThis.innerHTML = dom;
-        targetThis.addClass = (classText: string): void => targetThis.classList.add(classText);
-        targetThis.removeClass = (classText: string): void => targetThis.classList.remove(classText);
-        targetThis.toggleClass = (classText: string): void => targetThis.classList.toggle(classText); // 更新方法 2021/09/20
-        targetThis.on = (eventType: string, fn: Function): void => { targetThis[["on", eventType].join("")] = (t: Event) => fn.call(targetThis, t); }; // 更新方法 2021/09/20
-        targetThis.listener = (eventType: string, fn: Function): void => targetThis.addEventListener(eventType, fn);
-        targetThis.removeListener = (eventType: string, fn: Function): void => targetThis.removeEventListener(eventType, fn); // 更新方法 2022/01/04
-        targetThis.val = (valTemp: string | undefined): string | void => valTemp === undefined ? targetThis.value : targetThis.value = valTemp;
-        targetThis.attr = (props: string, val: any): string | number | void => val === undefined ? targetThis.getAttribute(props) : targetThis.setAttribute(props, val);
-        targetThis.props = (props: string, val: any): any => val === undefined ? targetThis[props] : targetThis[props] = val;
-        targetThis.styles = (method: string, cssType: string, cssParameter: string): void => { if (!$.includes(['set', 'remove'], method)) { $.console('error', "First parameter method must use string and keyword is 'set' or 'remove'."); return; }; method === 'set' ? targetThis.style.setProperty(cssType, cssParameter) : targetThis.style.removeProperty(cssType); }; // 更新方法 2021/10/26
-        targetThis.getDomStyles = (conditionProps: string[]): { [key: string]: any } => { let cssProperty: { [key: string]: any } = {}; if (typeof conditionProps !== "object") { $.console('error', 'Parameter must use array.'); return; } else { if (conditionProps.length === 0) { $.console('error', 'Parameter must use array,and css property must in array with string.'); return; } else { $.each(conditionProps, item => cssProperty[item] = getComputedStyle($(targetThis)).getPropertyValue(item)); return cssProperty; } }; }; // 更新方法 2021/10/26
-        targetThis.sibling = (num: number): HTMLElement => $(targetThis[num]);         // 更新方法 2021/08/31
-        targetThis.child = (num: number): HTMLElement => $(targetThis.children[num]);  // 更新方法 2021/08/31
-        targetThis.childFirst = (): HTMLElement => $(targetThis.firstElementChild);    // 更新方法 2021/08/31
-        targetThis.childLast = (): HTMLElement => $(targetThis.lastElementChild);      // 更新方法 2021/08/31
-        targetThis.parent = (): HTMLElement => $(targetThis.parentNode);               // 更新方法 2021/08/31
-        targetThis.appendDom = (el: HTMLElement): void => $(targetThis).append(el);    // 更新方法 2021/09/12
-        targetThis.removeDom = (): void => $(targetThis).remove();                     // 更新方法 2021/09/12
-        targetThis.removeChildDom = (): void => $(targetThis).replaceChildren();       // 更新方法 2021/10/25
-        targetThis.appendDomText = (el: Text): void => $(targetThis).appendChild(el);  // 更新方法 2021/09/12
-        targetThis.easyAppendDom = (orderBy: string, domStr: string): void => $(targetThis).insertAdjacentHTML(orderBy !== 'afterDom' ? 'afterbegin' : 'beforeend', domStr);  // 更新方法 2021/11/25
-        targetThis.scrollToTop = (scrollSetting: { [key: string]: number, scrollTop: number, duration: number } = { scrollTop: 0, duration: 0 }): void => { // 更新方法 2021/10/26
+    const $ = (target: any): any => {
+        let self: any = el.call(el, target) || target;
+        self.texts = (txt: string | undefined): string | void => txt === undefined ? self.textContent : self.textContent = txt;
+        self.html = (dom: string | undefined): string | void => dom === undefined ? self.innerHTML : self.innerHTML = dom;
+        self.addClass = (classText: string): void => self.classList.add(classText);
+        self.removeClass = (classText: string): void => self.classList.remove(classText);
+        self.toggleClass = (classText: string): void => self.classList.toggle(classText); // 更新方法 2021/09/20
+        self.on = (eventType: string, fn: Function): void => { self[["on", eventType].join("")] = (t: Event) => fn.call(self, t); }; // 更新方法 2021/09/20
+        self.listener = (eventType: string, fn: Function): void => self.addEventListener(eventType, fn);
+        self.removeListener = (eventType: string, fn: Function): void => self.removeEventListener(eventType, fn); // 更新方法 2022/01/04
+        self.val = (valTemp: string | undefined): string | void => valTemp === undefined ? self.value : self.value = valTemp;
+        self.attr = (props: string, val: any): string | number | void => val === undefined ? self.getAttribute(props) : self.setAttribute(props, val);
+        self.props = (props: string, val: any): any => val === undefined ? self[props] : self[props] = val;
+        self.sibling = (num: number): HTMLElement => $(self[num]);         // 更新方法 2021/08/31
+        self.child = (num: number): HTMLElement => $(self.children[num]);  // 更新方法 2021/08/31
+        self.childFirst = (): HTMLElement => $(self.firstElementChild);    // 更新方法 2021/08/31
+        self.childLast = (): HTMLElement => $(self.lastElementChild);      // 更新方法 2021/08/31
+        self.parent = (): HTMLElement => $(self.parentNode);               // 更新方法 2021/08/31
+        self.appendDom = (el: HTMLElement): void => $(self).append(el);    // 更新方法 2021/09/12
+        self.removeDom = (): void => $(self).remove();                     // 更新方法 2021/09/12
+        self.removeChildDom = (): void => $(self).replaceChildren();       // 更新方法 2021/10/25
+        self.appendDomText = (el: Text): void => $(self).appendChild(el);  // 更新方法 2021/09/12
+        self.easyAppendDom = (orderBy: string, domStr: string): void => $(self).insertAdjacentHTML(orderBy !== 'afterDom' ? 'afterbegin' : 'beforeend', domStr);  // 更新方法 2021/11/25
+        self.styles = (method: string, cssType: string, cssParameter: string): void => { // 更新方法 2021/10/26
+            if (!$.includes(['set', 'remove'], method)) {
+                $.console('error', "First parameter method must use string and keyword is 'set' or 'remove'.");
+                return;
+            };
+            method === 'set' ? self.style.setProperty(cssType, cssParameter) : self.style.removeProperty(cssType);
+            return self
+        };
+        self.getDomStyles = (conditionProps: string[]): { [key: string]: any } => { // 更新方法 2021/10/26
+            let cssProperty: { [key: string]: any } = {};
+            if (typeof conditionProps !== "object") {
+                $.console('error', 'Parameter must use array.');
+                return;
+            } else {
+                if (conditionProps.length === 0) {
+                    $.console('error', 'Parameter must use array,and css property must in array with string.');
+                    return;
+                } else {
+                    $.each(conditionProps, item => cssProperty[item] = getComputedStyle($(self)).getPropertyValue(item));
+                    return cssProperty;
+                }
+            };
+        };
+        self.scrollToTop = (scrollSetting: { [key: string]: number, scrollTop: number, duration: number } = { scrollTop: 0, duration: 0 }): void => { // 更新方法 2021/10/26
             let animateScroll: any = undefined;
             const [keyI, keyII]: string[] = Object.keys(scrollSetting);
-            const startPos: number = targetThis[keyI];
+            const startPos: number = self[keyI];
             const changePos: number = scrollSetting[keyI] - startPos;
             const startTimeStamp: number = +new Date();
 
@@ -53,16 +73,16 @@ const $: any = (function (el) {
 
             (animateScroll = () => {
                 const currentTimeStamp: number = +new Date() - startTimeStamp;
-                targetThis.scrollTop = Number(animationSettings({
+                self.scrollTop = Number(animationSettings({
                     currentTime: currentTimeStamp,
                     startVal: startPos,
                     changeVal: changePos,
                     animateDuration: scrollSetting[keyII]
                 }));
-                currentTimeStamp < scrollSetting[keyII] ? requestAnimationFrame(animateScroll) : targetThis.scrollTop = scrollSetting[keyI];
+                currentTimeStamp < scrollSetting[keyII] ? requestAnimationFrame(animateScroll) : self.scrollTop = scrollSetting[keyI];
             })();
         }
-        return targetThis;
+        return self;
     };
 
     $.each = (item: any[], fn: (...parameters: any[]) => void): void => item.forEach((items: any, index: number) => fn.call(item, items, index));
@@ -160,7 +180,7 @@ const $: any = (function (el) {
         }
     }
 
-    $.formatDateTime = (format: { formatDate: string | Date, formatType: string, localCountryTime?: number }): string | number => { // 更新方法 2021/12/01
+    $.formatDateTime = (format: { formatDate: string | Date, formatType: string, localCountryTime?: number, toDateFullNumber?: boolean }): string | number => { // 更新方法 2021/12/01
         //#region 參數設定
         /**
          * @param {object}
@@ -168,6 +188,7 @@ const $: any = (function (el) {
          *   formatDate: Date || string,
          *   formatType:string, <= formatType 參數 time 取時間、date 取日期、full 取日期與時間
          *   localCountryTime:number <= localCountryTime 根據時區格式化，預設為 GMT+8，可選參數
+         *   toDateFullNumber <= toDateFullNumber 將當前格式化時間改為數字，可以用於排序上，可選參數
          * }
          * @returns {string}
          */
@@ -186,21 +207,20 @@ const $: any = (function (el) {
         const dateSplit: string[] = dateStr.replace(/T/g, "-").replace(/:/g, "-").split(".")[0].split("-");
         const [year, month, date, hour, minute, second] = dateSplit;
 
-        return {
-            date: `${year}-${month}-${date}`,
-            time: `${hour}：${minute}：${second}`,
-            full: `${year}-${month}-${date} ${hour}：${minute}：${second}`,
-            toDateFullNumber: Number(dateSplit.join(""))
-        }[format.formatType]
+        if ('toDateFullNumber' in format) {
+            return format.toDateFullNumber ? Number(dateSplit.join("")) : format.formatType.replace(/yyyy/g, year).replace(/MM/g, month).replace(/dd/g, date).replace(/HH/g, hour).replace(/mm/g, minute).replace(/ss/g, second)
+        }
+
+        return format.formatType.replace(/yyyy/g, year).replace(/MM/g, month).replace(/dd/g, date).replace(/HH/g, hour).replace(/mm/g, minute).replace(/ss/g, second)
     }
 
     $.fetch = async (settingParams: {
         method: string,
         url: string,
-        headers: { [key: string]: any } | undefined
-        contentType: string,
-        data: { [key: string]: any } | undefined,
-        beforePost: Function,
+        headers?: { [key: string]: any },
+        contentType?: string,
+        data?: { [key: string]: any },
+        beforePost?: Function,
         successFn: Function,
         errorFn: Function
     }): Promise<void> => {
@@ -213,9 +233,9 @@ const $: any = (function (el) {
          * @param {string} url
          * @param {object} header 追加 hearder 物件 2021/10/21
          * @param {string} contentType
-         * @param {Function} beforePost <= 回呼函式
-         * @param {Function} successFn <= 回呼函式
-         * @param {Function} errorFn <= 回呼函式
+         * @param {Function} beforePost <= 回調函式
+         * @param {Function} successFn <= 回調函式
+         * @param {Function} errorFn <= 回調函式
          */
         //#endregion
 
@@ -228,11 +248,15 @@ const $: any = (function (el) {
 
         if (headers !== undefined) {
             settings.headers = headers;
-        } else if (data !== undefined) {
+        }
+
+        if (data !== undefined) {
             settings.headers = { "Content-Type": contentType };
             settings.body = $.convert(data, 'stringify');
-        } else if (headers !== undefined && data !== undefined) {
-            settings.headers = { ...headers, "Content-Type": contentType };
+        }
+
+        if (headers !== undefined && data !== undefined) {
+            settings.headers = { ...headers };
             settings.body = $.convert(data, 'stringify');
         };
 
@@ -268,12 +292,13 @@ const $: any = (function (el) {
     return $;
 }((el: object | string): any => typeof el === "object" ? el : document.querySelectorAll(el).length > 1 ? document.querySelectorAll(el) : document.querySelector(el))); // 更新元素指向 2021/8/31
 
-// use in node js
+// Use in node js
 // declare global {
-//     interface Date { calculateDay: (format: { day:number,method: string }) => Date }
+//     interface Date { calculateDay: (format: { day: number, method: string }) => Date }
 // }
 
 interface Date { calculateDay: (format: { day: number, method: string }) => Date }
+
 Date.prototype.calculateDay = (format: { day: number, method: string }) => {
     // 更新方法內容與回傳內容 2021/09/22
     // 更新方法 2021/12/01
